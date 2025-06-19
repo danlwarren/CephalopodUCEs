2d333fa782af15522f2a598dfaa0fa6eae408bfa
roblanf <rob.lanfear@gmail.com>
2025-06-20 09:09:01 +1000

    Final tree

--- 6_phylogenetic_analysis/README.md
+++ 6_phylogenetic_analysis/README.md
@@ -1,1 +1,71 @@
-# CephalopodUCEs
+# Phylogenetic analyses
+
+Here we use the final cleaned alignments from `5_final_alignments/3_final_alignments` to build various trees. This follows the concordance vector tutorial from the IQ-TREE docs for the most part.
+
+Note that here I first edited the filenames of the final alignments exported from Geneious to remove the dashes and `(modified)` suffixes, because IQ-TREE doesn't like special characters in filenames.
+
+### 1. Get set up
+
+```bash
+conda create --name concordance
+conda activate concordance
+
+# install what we need for this recipe
+conda install -c bioconda iqtree astral-tree
+conda install -c conda-forge r-base r-tidyverse r-boot r-ape r-ggtext
+
+# get the R script
+wget https://raw.githubusercontent.com/roblanf/concordance_vectors/main/concordance_vector.R
+wget https://raw.githubusercontent.com/roblanf/concordance_vectors/main/concordance_table.R
+wget https://raw.githubusercontent.com/roblanf/concordance_vectors/main/change_labels.R
+```
+
+### 2. Run gene trees
+
+```bash
+iq3=../3_initial_alignment/gene_trees/iqtree-3.0.1-Linux/bin/iqtree3
+aln=../5_final_alignments/3_final_alignments/
+$iq3 -S $aln -m MFP --prefix loci -T 128
+```
+
+### 3. Estimate the species tree
+
+Given that we have UCEs, and that stochastic error in gene trees will be extremely high, we will estimate the species tree using a partitioned model on a concatenated dataset as follows:
+
+```bash
+$iq3 -p $aln --prefix concat_species_tree -m MFP+MERGE -B 1000 -T 32
+```
+
+### 4. Get the qCFs from ASTRAL
+
+Here we map the gene trees to the species tree using ASTRAL to get the qCF values
+
+```bash
+astral -q concat_species_tree.treefile -i loci.treefile -t 2 -o astral_species_annotated.tree 2> astral_species_annotated.log
+```
+
+### 5. Get the gCF and sCF values with IQ-TREE
+
+Since I estimated a partitioning scheme first, I use that here. 
+
+```bash
+# sCF
+$iq3 -te astral_species_annotated.tree -p concat_species_tree.best_scheme.nex --scfl 100 --prefix scfl -T 128
+
+# gCF
+$iq3 -te scfl.cf.tree --gcf loci.treefile --prefix gcf -T 128
+
+# coalescent branch lengths
+$iq3 -te astral_species_annotated.tree -blfix -p loci.best_model.nex --scfl 1 --prefix coalescent_bl -T 128
+
+```
+
+### 6. Process the data to get concordance vectors
+
+Here we just process the branch labels to put them in various more convenient formats.
+
+```bash
+Rscript concordance_vector.R
+Rscript change_labels.R
+
+```
---
+++ 6_phylogenetic_analysis/concordance_table.R
@@ -0,0 +1,147 @@
+#!/usr/bin/env Rscript
+
+# Load necessary libraries
+library(tidyverse)
+library(ggplot2)
+library(boot)
+library(ggtext)
+
+bootstrap_ci_counts <- function(counts) {
+    
+    # make a vector of integers 1, 2, 3 and 4 for the 4 psi's
+    # we round the numbers becuase some estimates of CFs are not integers
+    data <- rep(1:length(counts), round(counts))
+    
+    count_func <- function(data, indices) {
+        sample_data <- data[indices]
+        counts <- sapply(1:length(counts), function(x) sum(sample_data == x))
+        return(counts)
+    }
+    
+    # Perform bootstrap
+    results <- boot(data, statistic = count_func, R = 1000)
+    
+    # Calculate 95% confidence intervals for each category
+    ci_results <- tibble(
+        lower_ci_count = apply(results$t, 2, quantile, probs = 0.025),
+        upper_ci_count = apply(results$t, 2, quantile, probs = 0.975)
+    )
+    
+    return(ci_results)
+}
+
+# Define the create_heatmap function with bootstrap CIs
+create_heatmap <- function(branch_id, conc_vectors) {
+    # Filter and select data based on branch ID
+    clade_data <- conc_vectors %>%
+        filter(ID == branch_id) %>%
+        select(gene_psi1, gene_psi2, gene_psi3, gene_psi4, site_psi1, site_psi2, site_psi3, site_psi4, quartet_psi1, quartet_psi2, quartet_psi3, quartet_psi4,
+               gene_psi1_N, gene_psi2_N, gene_psi3_N, gene_psi4_N, site_psi1_N, site_psi2_N, site_psi3_N, site_psi4_N, quartet_psi1_N, quartet_psi2_N, quartet_psi3_N, quartet_psi4_N)
+    
+    # Reshape data from wide to long format for proportions
+    long_data <- clade_data %>%
+        select(-ends_with("_N")) %>%
+        pivot_longer(cols = everything(), names_to = "type_psi", values_to = "value") %>%
+        separate(type_psi, into = c("type", "psi"), sep = "_psi") %>%
+        mutate(type = factor(type, levels = c("quartet", "site", "gene")))
+    
+    # Reshape data from wide to long format for counts
+    long_data_N <- clade_data %>%
+        select(ends_with("_N")) %>%
+        pivot_longer(cols = everything(), names_to = "type_psi_N", values_to = "count") %>%
+        separate(type_psi_N, into = c("type", "psi_N"), sep = "_psi") %>%
+        mutate(type = factor(type, levels = c("quartet", "site", "gene")))
+    
+    # Calculate bootstrap 95% CIs on the concordance vectors
+    long_data_N <- long_data_N %>%
+        group_by(type) %>%
+        group_modify(~ {
+            counts <- .x$count
+            total_counts <- sum(counts)
+            ci <- bootstrap_ci_counts(counts)
+            ci <- ci %>%
+                mutate(lower_CI = (lower_ci_count / total_counts) * 100,
+                       upper_CI = (upper_ci_count / total_counts) * 100,
+                       total_counts = total_counts) # Add total_counts to ci
+            
+            .x <- .x %>% bind_cols(ci)
+            return(.x)
+        }) %>%
+        ungroup()
+    
+    # Add bootstrap CIs to the long_data table in percentage terms
+    long_data <- long_data %>%
+        mutate(psi = paste0(psi, "_N")) %>%
+        left_join(long_data_N %>% select(type, psi_N, lower_CI, upper_CI, total_counts), by = c("type", "psi" = "psi_N")) %>%
+        mutate(psi = str_replace(psi, "_N", ""),
+               main_label = scales::number(value, accuracy = 0.001),
+               ci_label = paste0("(", scales::number(lower_CI, accuracy = 0.001), ", ", 
+                                 scales::number(upper_CI, accuracy = 0.001), ")"))
+
+    
+    y_labels <- long_data %>%
+        select(type, total_counts) %>%
+        distinct() %>%
+        mutate(y_label = paste0(type, "<br><span style='font-size:8pt;'>(n=", total_counts, ")</span>")) %>%
+        arrange(factor(type, levels = c("quartet", "site", "gene"))) %>% 
+        pull(y_label)
+    
+        
+    # Generate the heatmap
+    plot <- ggplot(long_data, aes(x = psi, y = type, fill = value)) +
+        geom_tile(color = "white") + # Add tiles with white borders
+        geom_text(aes(label = main_label), color = "black", size = 6, vjust = -0.5) + # Add main number text labels
+        geom_text(aes(label = ci_label), color = "black", size = 3, vjust = 1.5) + # Add CI text labels
+        scale_fill_gradient(low = "white", high = "red", limits = c(0, 100), name = "proportion") + # Color gradient
+        scale_x_discrete(position = 'top', labels = c(bquote(Psi[1]), bquote(Psi[2]), bquote(Psi[3]), bquote(Psi[4]))) +
+        scale_y_discrete(labels = y_labels) +
+        theme_minimal() + # Minimal theme
+        ggtitle(paste("Concordance factors for branch ID", branch_id), subtitle = "Values are percentages, numbers in brackets are bootstrap 95% CIs") +
+        theme(
+            plot.title = element_text(size = 24),
+            axis.title = element_blank(), # Remove axis titles
+            axis.text.y = element_markdown(size = 16), # Use element_markdown for y-axis labels
+            axis.text.x = element_text(size = 16), # Increase x-axis text size
+            axis.ticks = element_blank(),
+            axis.line = element_blank(),
+            line = element_blank(),
+            legend.position = "bottom" # Place legend at the bottom
+        ) +
+        guides(fill = guide_colourbar(
+            title = "Concordance or Discordance factor (%)",
+            title.position = "top",
+            title.hjust = 0.5,
+            barwidth = unit(8, "cm"),
+            barheight = unit(0.5, "cm"),
+            label.position = "bottom",
+            label.theme = element_text(size = 14),
+            ticks.colour = "black"
+        ))
+    
+    output_data <- long_data %>%
+        select(type, psi, value, lower_CI, upper_CI)
+    
+    return(list(plot = plot, data = output_data))
+}
+
+# Main script
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) < 2) {
+    stop("Please provide both the concordance vectors CSV file and a branch ID as command line arguments.")
+}
+
+csv_file <- args[1]
+branch_id <- as.numeric(args[2])
+
+# Load the input data
+concordance_vectors <- read_csv(csv_file)
+
+output <- create_heatmap(branch_id, concordance_vectors)
+
+# Save the plot to a PDF file
+pdf_filename <- paste0("concordance_table_", branch_id, ".pdf")
+ggsave(pdf_filename, plot = output$plot, width = 8, height = 5)
+
+# Save the long_data table to a CSV file
+csv_filename <- paste0("concordance_table_", branch_id, ".csv")
+write_csv(output$data, csv_filename)
