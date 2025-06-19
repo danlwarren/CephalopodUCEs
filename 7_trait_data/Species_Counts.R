93006486de3c537f7caf4aa07d24f4eeaec9c389
Dan Warren <dan.l.warren@gmail.com>
2025-06-19 12:13:28 +1000

    Readme for species counts

--- 7_trait_data/Species_Counts.R
+++ 7_trait_data/Species_Counts.R
@@ -46,26 +46,43 @@ families <- c("Alloposidae",
 family.df <- do.call(rbind, wm_records_taxamatch(families))
 family.df <- family.df %>%
   filter(AphiaID == valid_AphiaID)
-  
+
+subfamily.df <- NULL
+for(i in 1:nrow(family.df)){
+  subfamily.df <- rbind(subfamily.df, wm_children(as.numeric(family.df[i, "AphiaID"])))
+}
+
+subfamily.df <- subfamily.df %>%
+  filter(rank == "Subfamily" & valid_AphiaID == AphiaID)
+
 genera.df <- NULL
 for(i in 1:nrow(family.df)){
   genera.df <- rbind(genera.df, wm_children(as.numeric(family.df[i, "AphiaID"])))
 }
 
+for(i in 1:nrow(subfamily.df)){
+  genera.df <- rbind(genera.df, wm_children(as.numeric(subfamily.df[i, "AphiaID"])))
+}
+
 genera.df <- genera.df %>%
   filter(rank == "Genus" & valid_AphiaID == AphiaID)
 
-species.df <- NULL
+raw.species.df <- NULL
 for(i in 1:nrow(genera.df)){
-  species.df <- rbind(species.df, wm_children(as.numeric(genera.df[i, "AphiaID"])))
+  raw.species.df <- rbind(raw.species.df, wm_children(as.numeric(genera.df[i, "AphiaID"])))
 }
 
-species.df <- species.df %>%
-  filter(rank == "Species" & valid_AphiaID == AphiaID & status == "accepted")
+write.csv(raw.species.df, "raw_worms_species_data.csv", row.names = FALSE)
+
+species.df <- raw.species.df %>%
+  filter(rank == "Species" & 
+           valid_AphiaID == AphiaID & 
+           status == "accepted" & 
+           !is.na(valid_name))
 
 count.df <- species.df %>% 
   group_by(family) %>%
   count()
 
-write.csv(species.df, "species.csv", row.names = FALSE)
+write.csv(species.df, "filtered_species.csv", row.names = FALSE)
 write.csv(count.df, "species_counts.csv", row.names = FALSE)
